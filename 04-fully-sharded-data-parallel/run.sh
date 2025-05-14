60e9ffa886f714d6d08456a59299db0940661f0f
Galen Shipman <gshipman@lanl.gov>
2025-05-14 16:10:30 -0600

    add run scripts and see if 04 example will run on CPU, currently not as sharded may only work on GPU?

---
+++ 04-fully-sharded-data-parallel/run.sh
@@ -0,0 +1,14 @@
+export TORCHELASTIC_ERROR_FILE=../error.json
+export OMP_NUM_THREADS=64
+export HF_HOME=../.cache
+
+torchrun --standalone \
+    --nnodes 1 \
+    --nproc-per-node 2 \
+    --redirects 3 \
+    --log-dir ../logs \
+    train_llm.py \
+    --experiment-name fsdp \
+    --dataset-name tatsu-lab/alpaca \
+    --model-name openai-community/gpt2 \
+    --cpu-offload off
